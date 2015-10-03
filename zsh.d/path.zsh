40a1f134279eb8d4ca0f05b403f0222d9e1e4876
Koen Wilde <koen@chillheid.nl>
2015-10-03 23:04:05 +0200

    [zsh] Make sure /sbin and /bin are in $PATH
    These dirs still exist on Debian.

--- zsh.d/path.zsh
+++ zsh.d/path.zsh
@@ -1,4 +1,4 @@
-for dir in /usr/bin /usr/sbin; do
+for dir in /usr/bin /usr/sbin /sbin /bin; do
   if test -d "$dir" && ! grep -q -e "^${dir}:" -e ":${dir}:" -e ":${dir}" <<< $PATH; then
     PATH="${dir}:${PATH}"
   fi
