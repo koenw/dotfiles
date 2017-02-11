97620101965385d829be3a304e015eb327ee03ec
Koen Wilde <koen@chillheid.nl>
2017-02-11 21:33:34 +0100

    Add awesome config

---
+++ config/awesome/rc.lua
@@ -0,0 +1,26 @@
+local awful = require("awful")
+local naughty = require("naughty")
+
+confdir = awful.util.getdir("config")
+local rc, err = loadfile(confdir .. "/awesome.lua");
+if rc then
+    rc, err = pcall(rc);
+    if rc then
+        return;
+    end
+end
+
+-- Save output to /tmp for config troubleshooting
+-- output = io.open("/tmp/awesome_output", 'wb')
+-- output:write(err)
+-- output:flush()
+-- output:close()
+
+dofile("/etc/xdg/awesome/rc.lua");
+
+for s = 1,screen.count() do
+    mypromptbox[s].text = awful.util.escape(err:match("[^\n]*"));
+end
+
+naughty.notify{text="Awesome crashed during startup on "
+.. err .. "\n", timeout = 0}
