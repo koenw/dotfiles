1a438d013cba4426e2c134755018637ee8d15351
Koen Wilde <koen@chillheid.nl>
2017-04-30 23:42:56 +0200

    [ansible] Enable titlebars
    This makes it easier to see what window has focus on 4k screens ;)

--- awesome/awesome.lua
+++ awesome/awesome.lua
@@ -492,6 +492,9 @@ awful.rules.rules = {
 --     { rule_any = {type = { "normal", "dialog" }
 --       }, properties = { titlebars_enabled = true }
 --     },
+    { rule_any = {type = { "normal", "dialog" }
+      }, properties = { titlebars_enabled = true }
+    },
 
     -- Set Firefox to always map on the tag named "2" on screen 1.
     -- { rule = { class = "Firefox" },
