6e0556d666d20671fad163209ac28d45a7cb64e1
Koen Wilde <koen@chillheid.nl>
2017-08-07 13:43:15 +0200

    awesome: Set nice wallpaper

--- awesome/awesome.lua
+++ awesome/awesome.lua
@@ -42,6 +42,7 @@ end
 -- {{{ Variable definitions
 -- Themes define colours, icons, font and wallpapers.
 beautiful.init(awful.util.get_themes_dir() .. "zenburn/theme.lua")
+beautiful.wallpaper = awful.util.get_themes_dir() .. "wallpaper.png"
 
 -- This is used later as the default terminal and editor to run.
 terminal = "urxvtc"
