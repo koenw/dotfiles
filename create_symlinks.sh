396cf0f3573a669f4db973694f7abb384a856534
Koen Wilde <koen@chillheid.nl>
2018-01-04 10:46:02 +0100

    Use more beautiful (zenburn) colors for ls

--- create_symlinks.sh
+++ create_symlinks.sh
@@ -21,7 +21,7 @@ set -eu
 #files=(gnupg htoprc tmux.conf vimrc vim tmux.conf Xresources zshrc zsh.d xinitrc gitconfig)
 files=(htoprc tmux.conf vimrc vim tmux.conf Xresources zshrc zsh.d xinitrc gitconfig gtkrc-2.0 krb5.conf)
 ## Directories in ${HOME}/.config to create symlinks to
-dot_config_dirs=(awesome)
+dot_config_dirs=(awesome dircolors)
 ## Don't prompt before removing current file
 FORCE=false
 ## The target dir can be relative to $HOME
