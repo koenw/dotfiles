0a83b51d194ff8f93fb8027ab403608d9fa3230b
Koen Wilde <koen@chillheid.nl>
2018-02-01 11:48:53 +0100

    Fix awesome background

--- awesome/awesome.lua
+++ awesome/awesome.lua
@@ -41,8 +41,8 @@ end
 
 -- {{{ Variable definitions
 -- Themes define colours, icons, font and wallpapers.
-beautiful.init(awful.util.get_themes_dir() .. "zenburn/theme.lua")
-beautiful.wallpaper = awful.util.get_themes_dir() .. "wallpaper.png"
+beautiful.init(gears.filesystem.get_themes_dir() .. "zenburn/theme.lua")
+beautiful.wallpaper = "/home/koen/.config/awesome/themes/wallpaper.png"
 
 -- This is used later as the default terminal and editor to run.
 -- terminal = "urxvtc"
