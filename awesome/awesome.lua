c7440cbee50e6ed347b34589f910f5356d66ee98
Koen Wilde <koen@chillheid.nl>
2018-06-03 18:43:50 +0200

    Change order of layouts in awesome
    Turns out I rather like the corner.nw :)

--- awesome/awesome.lua
+++ awesome/awesome.lua
@@ -61,6 +61,7 @@ modkey = "Mod4"
 
 -- Table of layouts to cover with awful.layout.inc, order matters.
 awful.layout.layouts = {
+    awful.layout.suit.corner.nw,
     awful.layout.suit.tile,
     awful.layout.suit.tile.left,
     awful.layout.suit.tile.bottom,
@@ -72,7 +73,6 @@ awful.layout.layouts = {
     awful.layout.suit.max,
     awful.layout.suit.max.fullscreen,
     awful.layout.suit.magnifier,
-    awful.layout.suit.corner.nw,
     awful.layout.suit.floating,
     -- awful.layout.suit.corner.ne,
     -- awful.layout.suit.corner.sw,
