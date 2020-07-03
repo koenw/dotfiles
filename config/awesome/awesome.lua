2840dcc4e2ce6937c40bfddc6110c6855b7e72cf
Koen Wilde <koen@chillheid.nl>
2020-07-03 20:14:05 +0200

    Disable window borders

--- config/awesome/awesome.lua
+++ config/awesome/awesome.lua
@@ -502,7 +502,8 @@ root.keys(globalkeys)
 awful.rules.rules = {
     -- All clients will match this rule.
     { rule = { },
-      properties = { border_width = beautiful.border_width,
+      properties = { --border_width = beautiful.border_width,
+                     border_width = 0,
                      border_color = beautiful.border_normal,
                      focus = awful.client.focus.filter,
                      raise = true,
