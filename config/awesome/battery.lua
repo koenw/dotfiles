e61e2ae5aa2bc9568cb755b7d0f8902ce3358d7a
Koen Wilde <koen@chillheid.nl>
2020-08-22 21:15:20 +0200

    Improve battery widget

--- config/awesome/battery.lua
+++ config/awesome/battery.lua
@@ -5,15 +5,32 @@
 local naughty = require("naughty")
 local beautiful = require("beautiful")
 
-function readBatFile(adapter, ...)
+function readFile(path)
+  file = io.open(path, "r")
+  if file then
+    local str = file:read()
+    file:close()
+    return str
+  end
+end
+
+-- Given the name of a power device (e.g. BAT0) and a list of metrics
+-- corresponding to files in `/sys/class/power_supply/*/`, return the value of
+-- the given metric.
+function getBatteryMetric(adapter, ...)
   local basepath = "/sys/class/power_supply/"..adapter.."/"
   for i, name in pairs({...}) do
-    file = io.open(basepath..name, "r")
-    if file then
-      local str = file:read()
-      file:close()
-      return str
+    result = readFile(basepath .. name)
+    if result then
+      return result
     end
+  end
+end
+
+function getPowerUsage(adapter)
+  local uwatts = readFile("/sys/class/power_supply/" .. adapter .. "/power_now")
+  if uwatts then
+    return uwatts
   end
 end
 
@@ -24,16 +41,24 @@ function batteryInfo(adapter)
     icon = ""
     percent = ""
   else
-    local cur = readBatFile(adapter, "charge_now", "energy_now")
-    local cap = readBatFile(adapter, "charge_full", "energy_full")
-    local sta = readBatFile(adapter, "status")
-    battery = math.floor(cur * 100 / cap)
+    local energy_now = getBatteryMetric(adapter, "charge_now", "energy_now")
+    local energy_full = getBatteryMetric(adapter, "charge_full", "energy_full")
+    local status = getBatteryMetric(adapter, "status")
+    uwatts = getPowerUsage(adapter)
+    power_usage = ""
+    if uwatts then
+      power_usage = uwatts * 10^-6
+    else
+      power_usage = "?"
+    end
 
-    if sta:match("Charging") then
-      icon = "⚡"
+    battery_percent = math.floor(energy_now / energy_full * 100)
+
+    if status:match("Charging") then
+      icon = "🔌"
       percent = "%"
 
-      if tonumber(battery) == 80 then
+      if tonumber(battery_percent) == 80 then
         naughty.notify({ title    = "Battery charged to 80%, unplug charger?"
                , text     = ""
                , timeout  = 5
@@ -42,12 +67,12 @@ function batteryInfo(adapter)
                , bg       = beautiful.bg_focus
         })
       end
-    elseif sta:match("Discharging") then
-      icon = ""
+    elseif status:match("Discharging") then
+      icon = "🔋"
       percent = "%"
-      if tonumber(battery) < 15 then
+      if tonumber(battery_percent) < 15 then
         naughty.notify({ title    = "Battery Warning"
-               , text     = "Battery low!".."  "..battery..percent.."  ".."left!"
+               , text     = "Battery low!".."  "..battery_percent..percent.."  ".."left!"
                , timeout  = 5
                , position = "top_right"
                , fg       = beautiful.fg_focus
@@ -55,7 +80,7 @@ function batteryInfo(adapter)
         })
       end
 
-      if tonumber(battery) == 40 then
+      if tonumber(battery_percent) == 40 then
         naughty.notify({ title    = "Battery down to 40%, plug charger?"
                , text     = ""
                , timeout  = 5
@@ -74,5 +99,5 @@ function batteryInfo(adapter)
     -- fix 'too many open files' bug on awesome 4.0
     fh:close()
   end
-  return " "..icon..battery..percent.." "
+  return " " .. icon .. battery_percent .. percent .. " " .. power_usage .. " W⚡"
 end
