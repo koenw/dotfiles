4f77b82b35ef26e1bc2595faf33bbc537b55f681
Koen Wilde <koen@chillheid.nl>
2021-06-07 22:15:47 +0200

    Improve zsh/tmux integration

--- zsh.d/40-always-tmux.zsh
+++ zsh.d/40-always-tmux.zsh
@@ -2,10 +2,16 @@ if [[ "$TERM" == screen* ]] || [[ -n "$TMUX" ]]; then
   return 1
 fi
 
-if tmux list-sessions && (tmux list-sessions | test -n "$0"); then
-  session_id=`date +%Y%m%d%H%M%S`
-  tmux new-session -d -t base -s $session_id
-  tmux attach-session -t $session_id \; set-option destroy-unattached
+animals=(🐈 🦄 🐮 🐖 🐽 🐗 🐪 🦙 🦘 🦒 🦏 🦛 🐹 🦔 🦇 🐨 🦃 🐦 🦆 🦉 🐸 🦜 🦚 🦩 🐙 🐛 🐝 🦠 🐝 🐡 🦖 🐊)
+
+if tmux list-sessions |grep -e '.'; then
+  # There is at least one tmux session running. We'll take the oldest, and
+  # create a new session in the same group (so all windows are available from
+  # all clients)
+  current_session_id=$(tmux list-sessions |tail -n 1 |awk -F: '{print $1}')
+  new_session_id=${animals[RANDOM % $#animals + 1]}
+  tmux new-session -d -t $current_session_id -s $new_session_id
+  tmux attach-session -t $new_session_id \; set-option destroy-unattached
 else
-  tmux new-session -s base
+  tmux new-session -s ${animals[RANDOM % $#animals + 1]}
 fi
