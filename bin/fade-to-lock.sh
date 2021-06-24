d2f8caa86911824bcd43b979944b686ad2c4e55b
Koen Wilde <koen@chillheid.nl>
2021-06-24 03:28:32 +0200

    Update fade-to-lock to use `light`

--- bin/fade-to-lock.sh
+++ bin/fade-to-lock.sh
@@ -1,26 +1,30 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 # xss-lock notifier to dim the screen before locking it after inactivity.
 
 # Brightness will be lowered to this value.
 min_brightness=0
-fade_time=5000
+fade_time=10
 fade_steps=300
 
 get_brightness() {
-  xbacklight -get
+  light
 }
 
 set_brightness() {
-  xbacklight -steps 1 -set $1
+   light -S $1
 }
 
 fade_brightness() {
-  xbacklight -time $fade_time -steps $fade_steps -set $1
+  for s in `seq $(light) -1 0`; do
+    light -S $s
+    sleep 0.05
+  done
 }
 
 trap 'exit 0' TERM INT
 trap "set_brightness $(get_brightness); kill %%" EXIT
+
 fade_brightness $min_brightness
 sleep infinity &
 wait
