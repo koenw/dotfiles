9c0154b25647a7e11544e5668f2af35100c3d53d
Koen Wilde <koen@chillheid.nl>
2021-06-24 03:38:20 +0200

    Cleanup ssh function

--- zsh.d/29-ssh.zsh
+++ zsh.d/29-ssh.zsh
@@ -1,14 +1,24 @@
+# Start ssh-agent if it's not running yet
 pgrep -u "$USER" ssh-agent >/dev/null || ssh-agent > ~/.ssh-agent-env
 test -z "$SSH_AUTH_SOCK" && eval "$(<~/.ssh-agent-env)" >/dev/null
 
+# This wrapper will make sure my favourite ssh keys are loaded.
 function ssh() {
-  keyLifeTime=2h
+  local keyLifeTime=2h
+  local keys_to_add=(koen@carb koen@koendalini)
+  local added_keys=$(ssh-add -l |awk '{print $3}')
 
-  for key in ~/.ssh/id_*~*.pub; do
-    ssh-key-added "$key" || _ssh-add -t $keyLifeTime "$key"
+  for key in $keys_to_add; do
+    if ! echo -e $added_keys | grep -q "$key"; then
+      # Just add all the keys
+      for key in ~/.ssh/id_*~*(.pub|.sh); do
+        ssh-key-added "$key" || _ssh-add -t $keyLifeTime "$key"
+      done
+      break
+    fi
   done
 
-  eval TERM=screen $(type -p ssh |awk '{print $3}') $*
+  eval $(type -p ssh |awk '{print $3}') $*
 }
 
 # ssh-key-added takes the path to a ssh private key and checks if it's already
