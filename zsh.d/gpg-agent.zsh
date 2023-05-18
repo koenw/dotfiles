e9c672f75b1df35330ac5228c79826133601f790
Koen Wilde <koen@chillheid.nl>
2023-05-18 02:52:35 +0200

    Experiment with gpg-agent as ssh-agent

--- zsh.d/gpg-agent.zsh
+++ zsh.d/gpg-agent.zsh
@@ -1,6 +1,13 @@
 if which gpg >/dev/null 2>&1; then
   export GPG_TTY=$(tty)
-  gpg-connect-agent /bye
+  export SSH_AUTH_SOCK=$(gpgconf --list-dirs agent-ssh-socket)
+  # We don't export e.g. SSH_AUTH_SOCK here, because the gpg-agent will only be
+  # used as ssh-agent for some some but not all hosts/keys. The ssh_config will
+  # point to the relevant socket.
+  #pgrep -u "$USER" gpg-agent >/dev/null || gpg-agent --daemon --enable-ssh-support > ~/.gpg-agent-env
 
-  echo UPDATESTARTUPTTY | gpg-connect-agent >/dev/null
+  function gpg() {
+    gpg-connect-agent UPDATESTARTUPTTY /bye >/dev/null;
+    eval $(type -p gpg |awk '{print $3}') $*
+  }
 fi
