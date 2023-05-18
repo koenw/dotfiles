00426d4b24dc0089a72b14ae1f7c867678046540
Koen Wilde <koen@chillheid.nl>
2023-05-18 11:53:40 +0200

    Tweak gpg-agent settings

--- zsh.d/gpg-agent.zsh
+++ zsh.d/gpg-agent.zsh
@@ -4,7 +4,7 @@ if which gpg >/dev/null 2>&1; then
   # We don't export e.g. SSH_AUTH_SOCK here, because the gpg-agent will only be
   # used as ssh-agent for some some but not all hosts/keys. The ssh_config will
   # point to the relevant socket.
-  #pgrep -u "$USER" gpg-agent >/dev/null || gpg-agent --daemon --enable-ssh-support > ~/.gpg-agent-env
+  pgrep -u "$USER" gpg-agent >/dev/null || gpg-agent --daemon --enable-ssh-support > ~/.gpg-agent-env
 
   function gpg() {
     gpg-connect-agent UPDATESTARTUPTTY /bye >/dev/null;
