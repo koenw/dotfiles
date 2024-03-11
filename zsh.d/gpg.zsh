691ee77b3cf7d6db527a904e00d343fdfc61c6d5
Koen Wilde <koen@chillheid.nl>
2024-03-11 23:23:12 +0100

    Tweak ssh/gpg/yubi interaction

--- zsh.d/gpg.zsh
+++ zsh.d/gpg.zsh
@@ -1,12 +1,12 @@
 if which gpg >/dev/null 2>&1; then
-  export GPG_TTY=$(tty)
   export SSH_AUTH_SOCK=$(gpgconf --list-dirs agent-ssh-socket)
-  pgrep -u "$USER" gpg-agent >/dev/null || gpg-agent --daemon --enable-ssh-support > ~/.gpg-agent-env
+  pgrep -u "$USER" gpg-agent >/dev/null || eval $(gpg-agent --daemon --enable-ssh-support) # > ~/.gpg-agent-env
 
   function gpg() {
+    export GPG_TTY=$(tty)
     # Let the gpg-agent know on which tty we are in case the pinentry-program
     # needs to ask us some private questions.
-    gpg-connect-agent UPDATESTARTUPTTY /bye >/dev/null &;
+    gpg-connect-agent UPDATESTARTUPTTY /bye >/dev/null;
     eval $(type -p gpg |awk '{print $3}') $*
   }
 fi
